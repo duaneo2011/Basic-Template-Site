d2fc17f91a4399cfcf2deeef7e7caf6712ac3120
duaneo2011 <68498616+duaneo2011@users.noreply.github.com>
2021-05-25 06:14:21 -0800

    Add files via upload

---
+++ Templated site/script.js
@@ -0,0 +1,9 @@
+
+
+const menuIcon = document.querySelector
+(".hamburger-menu");
+const navbar = document.querySelector('.navbar');
+
+menuIcon.addEventListener('click', ()=>{
+navbar.classList.toggle('change');
+});
